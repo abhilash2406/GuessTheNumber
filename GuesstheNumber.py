b537c82cb6e96a9328375d93f9df1aa2b59e2730
abhilashkumar66666@gmail.com <abhilashkumar66666@gmail.com>
2022-06-27 10:08:47 +0530

    changed the limit

--- GuesstheNumber.py
+++ GuesstheNumber.py
@@ -1,7 +1,7 @@
 # to import random module
 import random
 # to create a range of random numbers between 1-10
-n = random.randrange(1,100)
+n = random.randrange(1,150)
 # to take a user input to enter a number
 guess = int(input("Enter any Number: "))
 while n!= guess: # means if n is not equal to the input guess
